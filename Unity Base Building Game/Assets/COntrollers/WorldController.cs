4cf9cb0d36f0b64b2a34d75288bfb684caa86ba9
WhiteBl4ck <zukaboy2369@gmail.com>
2018-09-25 16:32:37 +0200

    First Commit

---
+++ Unity Base Building Game/Assets/COntrollers/WorldController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+    World world;
+	// Use this for initialization
+	void Start () {
+        world = new World();
+        for (int i = 0; i < world.Width; i++)
+        {
+            for (int j = 0; j < world.Height; j++)
+            {
+                GameObject tile_go = new GameObject();
+                tile_go.name = "Tile_" + i + "_" + j;
+                SpriteRenderer tile_sr = tile_go.AddComponent<SpriteRenderer>();
+
+                 
+            }
+        }
+
+        // Create 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Unity Base Building Game/Assets/Model/Tile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile  {
+
+    public enum TileType { Empty, Floor };
+
+    TileType type = TileType.Empty;
+
+
+    World world;
+    int x;
+    int y;
+
+    public TileType Type
+    {
+        get
+        {
+            return type;
+        }
+       
+    }
+
+    public Tile(World world, int x, int y)
+    {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+    }
+}
